a15f0055d76ea959592d74552992d061ba064253
Baptiste Leproux <leproux.baptiste@gmail.com>
2024-01-31 11:08:42 +0100

    mountain

--- app.config.ts
+++ app.config.ts
@@ -2,11 +2,16 @@ export default defineAppConfig({
   ui: {
     primary: 'green',
     gray: 'slate',
+
     footer: {
       bottom: {
         left: 'text-sm text-gray-500 dark:text-gray-400',
         wrapper: 'border-t border-gray-200 dark:border-gray-800'
       }
+    },
+
+    icons: {
+      search: 'i-simple-icons-4chan'
     }
   },
   seo: {
